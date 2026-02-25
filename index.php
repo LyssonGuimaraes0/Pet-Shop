5cd2c8fb9b90630be230def52f9a8381504f9df9
Alysson Guimarães <->
2026-02-25 16:52:09 -0300

    Primeiro commite

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+
+
+<?php include('./snippets/head.html') ?>
+
+
+<body>
+    <!--Navbar-->
+    <?php include('./snippets/navbar.html') ?>
+    <!--Banner-->
+    <?php include('./snippets/banner.html') ?>
+
+</body>
+
+</html>
